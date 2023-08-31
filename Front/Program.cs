e760a3c810d341d893f8ca6ea6e3522e3f9cb6f6
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-08-31 16:22:28 -0300

    Add MudBlazor to front project

--- Front/Program.cs
+++ Front/Program.cs
@@ -1,4 +1,5 @@
 using Front;
+using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();

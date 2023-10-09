acdf52aa2b26d5d7e41cfb36722fb0eeebd7c80a
ZaqueuCavalcante <zaqueudovale@gmail.com>
2023-10-09 20:59:09 -0300

    Just save

---
+++ Tests/Base/BunitTestContext.cs
@@ -0,0 +1,13 @@
+using Bunit;
+using NUnit.Framework;
+
+namespace Syki.Tests.Base;
+
+public abstract class BunitTestContext : TestContextWrapper
+{
+  [SetUp]
+  public void Setup() => TestContext = new Bunit.TestContext();
+
+  [TearDown]
+  public void TearDown() => TestContext?.Dispose();
+}

4a02d91d36646e902fa2e4beb43a8c4e609e96e5
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-01-03 11:34:29 -0300

    Add more front components simple tests

--- Tests/Base/BunitTestContext.cs
+++ Tests/Base/BunitTestContext.cs
@@ -1,5 +1,6 @@
 using Bunit;
 using NUnit.Framework;
+using MudBlazor.Services;
 
 namespace Syki.Tests.Base;
 
@@ -10,4 +11,12 @@ public abstract class BunitTestContext : TestContextWrapper
 
     [TearDown]
     public void TearDown() => TestContext?.Dispose();
+
+    public void AddMudServices()
+    {
+        Services.AddMudServices();
+        TestContext!.JSInterop.SetupVoid("mudPopover.initialize", _ => true);
+        TestContext!.JSInterop.SetupVoid("mudElementRef.saveFocus", _ => true);
+        TestContext!.JSInterop.SetupVoid("mudScrollManager.lockScroll", _ => true);
+    }
 }

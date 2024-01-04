6c77e362974109adbc78dc8fce34ae95b7302ccb
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-01-03 23:59:45 -0300

    Fix test

--- Shared/Extensions/GuidExtensions.cs
+++ Shared/Extensions/GuidExtensions.cs
@@ -1,3 +1,5 @@
+using QRCoder;
+
 namespace Syki.Shared;
 
 public static class GuidExtensions
@@ -6,5 +8,20 @@ public static class GuidExtensions
     {
         var justNumbers = guid.ToString().OnlyNumbers();
         return int.Parse(justNumbers[..8]);
+    }
+
+    public static Byte[] GenerateQrCodeBytes(string key, string email)
+    {
+        const string provider = "Syki";
+
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(
+            $"otpauth://totp/{provider}:{email}?secret={key}&issuer={provider}",
+            QRCodeGenerator.ECCLevel.Q
+        );
+        
+        var qrCode = new PngByteQRCode(qrCodeData);
+
+        return qrCode.GetGraphic(20);
     }
 }

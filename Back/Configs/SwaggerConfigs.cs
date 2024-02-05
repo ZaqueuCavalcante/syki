ecd172e1f3f0f0d24e9877dc41735de1276e4f50
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-02-04 22:11:35 -0300

    Add auth to swagger

--- Back/Configs/SwaggerConfigs.cs
+++ Back/Configs/SwaggerConfigs.cs
@@ -19,6 +19,30 @@ public static class SwaggerConfigs
                 License = new OpenApiLicense() { Name = "License", Url = new Uri("https://opensource.org/licenses/MIT") }
             });
 
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+
             options.DescribeAllParametersInCamelCase();
 
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
@@ -32,6 +56,7 @@ public static class SwaggerConfigs
         app.UseSwagger();
         app.UseSwaggerUI(options =>
         {
+            options.DocumentTitle = "Syki API";
             options.SwaggerEndpoint("/swagger/v1/swagger.json", "Syki 1.0");
             options.DefaultModelsExpandDepth(-1);
         });

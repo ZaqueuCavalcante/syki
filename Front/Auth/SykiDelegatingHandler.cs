88511fc51cc51c75aaf2198994fedcc07bfbdc9e
Zaqueu Cavalcante <zaqueudovale@gmail.com>
2024-02-17 15:52:22 -0300

    Update SykiDelegatingHandler.cs

--- Front/Auth/SykiDelegatingHandler.cs
+++ Front/Auth/SykiDelegatingHandler.cs
@@ -13,7 +13,6 @@ public class SykiDelegatingHandler : DelegatingHandler
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        await Task.Delay(2_000);
         request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
 
         var token = await _localStorage.GetItemAsync("AccessToken");

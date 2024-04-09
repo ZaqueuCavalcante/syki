f2fa55a04a93de34c216bc01a63f411d1ae9eaff
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-04-09 11:26:57 -0300

    Fix tests folders structure

--- Tests/Audit/AuditIntegrationTests.cs
+++ Tests/Audit/AuditIntegrationTests.cs
@@ -5,50 +5,39 @@ namespace Syki.Tests.Integration;
 
 public class AuditIntegrationTests : IntegrationTestBase
 {
-    [Test, Ignore("")]
+    [Test]
     public async Task Deve_auditar_a_criacao_de_um_campus()
     {
         // Arrange
-        var client = _factory.CreateClient();
-        var faculdade = await client.CreateInstitution();
-        await client.RegisterAndLogin(faculdade.Id, Academico);
-
-        var body = new CreateCampusIn { Name = "Agreste I", City = "Caruaru - PE" };
+        var client = await _factory.LoggedAsAcademico();
 
         // Act
         Configuration.AuditDisabled = false;
-        var campus = await client.PostAsync<CampusOut>("/campi", body);
+        var campus = await client.CreateCampus("Agreste I", "Caruaru - PE");
         Configuration.AuditDisabled = true;
 
         // Assert
         using var ctx = _factory.GetDbContext();
         var audit = await ctx.AuditLogs.FirstAsync(a => a.EntityId == campus.Id);
         audit.Action.Should().Be("Insert");
-        audit.FaculdadeId.Should().Be(faculdade.Id);
     }
 
-    [Test, Ignore("")]
+    [Test]
     public async Task Deve_auditar_a_atualizacao_de_um_campus()
     {
         // Arrange
-        var client = _factory.CreateClient();
-        var faculdade = await client.CreateInstitution("Nova Roma");
-        await client.RegisterAndLogin(faculdade.Id, Academico);
 
-        var body = new CreateCampusIn { Name = "Agreste I", City = "Caruaru - PE" };
-        var campus = await client.PostAsync<CampusOut>("/campi", body);
+        var client = await _factory.LoggedAsAcademico();
+        var campus = await client.CreateCampus("Agreste I", "Caruaru - PE");
 
         // Act
-        campus.Name = "Agreste II";
-        campus.City = "Bonito - PE";
         Configuration.AuditDisabled = false;
-        await client.PutAsync<CampusOut>("/campi", campus);
+        await client.UpdateCampus(campus.Id, "Agreste II", "Bonito - PE");
         Configuration.AuditDisabled = true;
 
         // Assert
         using var ctx = _factory.GetDbContext();
         var audit = await ctx.AuditLogs.FirstAsync(a => a.EntityId == campus.Id);
         audit.Action.Should().Be("Update");
-        audit.FaculdadeId.Should().Be(faculdade.Id);
     }
 }

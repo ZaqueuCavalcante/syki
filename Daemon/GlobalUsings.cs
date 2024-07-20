8d03ab303cd1145372021fb3729fc7b9c44a4678
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-07-19 22:03:40 -0300

    Deploy

--- Daemon/GlobalUsings.cs
+++ Daemon/GlobalUsings.cs
@@ -1,7 +1,5 @@
-global using OneOf;
 global using Syki.Shared;
 global using Syki.Back.Tasks;
 global using Syki.Back.Database;
 global using Syki.Back.Settings;
-global using Syki.Back.Exceptions;
 global using Microsoft.EntityFrameworkCore;

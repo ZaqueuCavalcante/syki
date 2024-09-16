413b2995956577f914d77f3b7f375ede87f58697
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-09-16 07:49:46 -0300

    Drop skip register feature

--- Back/Features/Cross/SkipUserRegister/SkipUserRegisterController.cs
+++ Back/Features/Cross/SkipUserRegister/SkipUserRegisterController.cs
@@ -1,15 +1,15 @@
-namespace Syki.Back.Features.Cross.SkipUserRegister;
+// namespace Syki.Back.Features.Cross.SkipUserRegister;
 
-[ApiController]
-[EnableRateLimiting("Small")]
-[Consumes("application/json"), Produces("application/json")]
-public class SkipUserRegisterController(SkipUserRegisterService service) : ControllerBase
-{
-    [HttpPost("skip-user-register")]
-    public async Task<IActionResult> Skip([FromBody] SkipRegisterLoginIn data)
-    {
-        var result = await service.Skip(data);
+// [ApiController]
+// [EnableRateLimiting("Small")]
+// [Consumes("application/json"), Produces("application/json")]
+// public class SkipUserRegisterController(SkipUserRegisterService service) : ControllerBase
+// {
+//     [HttpPost("skip-user-register")]
+//     public async Task<IActionResult> Skip([FromBody] SkipRegisterLoginIn data)
+//     {
+//         var result = await service.Skip(data);
 
-        return result.Match<IActionResult>(Ok, BadRequest);
-    }
-}
+//         return result.Match<IActionResult>(Ok, BadRequest);
+//     }
+// }

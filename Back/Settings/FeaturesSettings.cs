776200cb3fe88ac5ffe234755eafd3adfa91d427
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-02-27 11:56:28 -0300

    Test

--- Back/Settings/FeaturesSettings.cs
+++ Back/Settings/FeaturesSettings.cs
@@ -16,7 +16,7 @@ public class LoadFeatureFlagsFromDb(IServiceScopeFactory serviceScopeFactory, Fe
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        await Task.Delay(5_000, cancellationToken);
+        await Task.Delay(30_000, cancellationToken);
 
         using IServiceScope scope = serviceScopeFactory.CreateScope();
         var ctx = scope.ServiceProvider.GetRequiredService<SykiDbContext>();

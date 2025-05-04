0fc918d7edf1d9dd3ce3b5f37c0929e73dfaf3f6
“Gutto” <guttolaudie@gmail.com>
2025-05-03 22:38:29 -0300

    fix(cleaning and adjusting): :fire: removed mobile apps and modified Dockerfiles
    Changed the dockerfiles to use k8s in the future, created a folder called Docker to put all files related to docker, except docker compose

--- App/lib/configs/services_configs.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:app/auth/auth_service.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-void addServicesConfigs() {
-  Get.put(const FlutterSecureStorage());
-  Get.put(AuthService());
-}

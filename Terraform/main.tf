0e22678590b875d0d9ff3057d245f72cf8b230f8
Gutto <guttolaudie@gmail.com>
2025-05-20 15:12:06 -0300

    feat: update to digitalocean

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,17 +1,46 @@
-resource "random_password" "pg_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+resource "digitalocean_droplet" "app" {
+  image    = "ubuntu-22-04-x64"
+  name     = "syki-app"
+  region   = var.region
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.main.id]
+
+  user_data = templatefile("${path.module}/scripts/cloud-init.yaml", {
+    postgres_password      = var.postgres_password
+    grafana_admin_password = var.grafana_admin_password
+  })
 }
 
-resource "random_password" "grafana_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+resource "digitalocean_firewall" "app" {
+  name = "syki-firewall"
+
+  droplet_ids = [digitalocean_droplet.app.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "5001-5003"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "5432"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "3000"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
 
-resource "random_string" "acr_suffix" {
-  length  = 6
-  special = false
-  upper   = false
+data "digitalocean_ssh_key" "main" {
+  name = var.ssh_key_name
 }

fd033b1903ea5968717d5c34aa3078449a553c24
Gutto <guttolaudie@gmail.com>
2025-05-20 22:17:02 -0300

    fix: :fire: firewall fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,35 +11,35 @@ resource "digitalocean_droplet" "app" {
   })
 }
 
-resource "digitalocean_firewall" "app" {
-  name = "syki-firewall"
-
-  droplet_ids = [digitalocean_droplet.app.id]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "5001-5003"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "5432"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "3000"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-}
+#resource "digitalocean_firewall" "app" {
+#  name = "syki-firewall"
+#
+#  droplet_ids = [digitalocean_droplet.app.id]
+#
+#  inbound_rule {
+#    protocol         = "tcp"
+#    port_range       = "22"
+#    source_addresses = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol         = "tcp"
+#    port_range       = "5001-5003"
+#    source_addresses = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol         = "tcp"
+#    port_range       = "5432"
+#    source_addresses = ["0.0.0.0/0", "::/0"]
+#  }
+#
+#  inbound_rule {
+#    protocol         = "tcp"
+#    port_range       = "3000"
+#    source_addresses = ["0.0.0.0/0", "::/0"]
+#  }
+#}
 
 data "digitalocean_ssh_key" "main" {
   name = var.ssh_key_name

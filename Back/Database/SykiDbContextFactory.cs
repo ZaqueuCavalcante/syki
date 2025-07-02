9408eecacbf92cfd0834cac9adc1ba1db94177da
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-07-02 19:31:36 -0300

    Fix tests pipeline

--- Back/Database/SykiDbContextFactory.cs
+++ Back/Database/SykiDbContextFactory.cs
@@ -7,12 +7,6 @@ public class SykiDbContextFactory : IDesignTimeDbContextFactory<SykiDbContext>
 {
     public SykiDbContext CreateDbContext(string[] args)
     {
-        return new SykiDbContext(new(), NpgsqlDataSource.Create(GetAppConfiguration().Database().ConnectionString));
-    }
-
-    static IConfiguration GetAppConfiguration()
-    {
-        var builder = new ConfigurationBuilder().AddEnvironmentVariables();
-        return builder.Build();
+        return new SykiDbContext(new(), NpgsqlDataSource.Create("UserID=a;Password=b;Host=c;Port=5432;Database=d;"));
     }
 }

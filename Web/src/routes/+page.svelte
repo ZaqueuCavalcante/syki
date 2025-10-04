c21376398183be8f5a3cd56cc5157501ce470194
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-10-03 22:19:32 -0300

    Save

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -1,7 +1,20 @@
-
 <svelte:head>
 	<title>Syki</title>
 </svelte:head>
+
+<h1 class="text-3xl font-bold">
+	Gerencie sua instituição de ensino com <span class="text-sky-500 underline">maestria!</span>
+</h1>
+
+<div
+	class="flex my-20 mx-auto max-w-sm items-center gap-x-4 rounded-xl bg-white p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+>
+	<img class="size-14 shrink-0" src="/chat.svg" alt="Syki Chat Logo" />
+	<div>
+		<div class="text-xl font-medium text-black dark:text-white">Syki Chat</div>
+		<p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+	</div>
+</div>
 
 <p>Campi <a href="/academic/campi">campi</a></p>
 

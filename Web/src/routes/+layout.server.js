05827d5d76719b344754aeac0fb54ac44430a29d
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-10-28 22:05:50 -0300

    Try again

--- Web/src/routes/+layout.server.js
+++ Web/src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 export function load({ url }) {
   const siteTitle = "Syki";
   const siteDescription = "Sistema de Gestão Escolar Open Source";
-  const siteImage = "https://svelte.syki.com.br/favicon.svg";
+  const siteImage = "https://svelte.syki.com.br/logo-medium.png";
   const siteUrl = url.href;
 
   return { siteTitle, siteDescription, siteImage, siteUrl };

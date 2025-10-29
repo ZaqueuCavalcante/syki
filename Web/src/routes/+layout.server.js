a89abdace0e344f28d594618af1c6197a4b27e89
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-10-28 22:49:11 -0300

    Preview

--- Web/src/routes/+layout.server.js
+++ Web/src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 export function load({ url }) {
   const siteTitle = "Syki - Sistema de Gestão Escolar Open Source";
   const siteDescription = "Sistema de gestão escolar completo e open source, que pode ser usado por gestores, professores, alunos e pais.";
-  const siteImage = "https://svelte.syki.com.br/og_image.png";
+  const siteImage = "https://svelte.syki.com.br/preview.png";
   const siteUrl = url.href;
 
   return { siteTitle, siteDescription, siteImage, siteUrl };

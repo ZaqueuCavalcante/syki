36f5959cf660e1831ef67ddffcf737665d783d52
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-10-28 22:28:40 -0300

    Add custom og:image

--- Web/src/routes/+layout.server.js
+++ Web/src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 export function load({ url }) {
   const siteTitle = "Syki";
   const siteDescription = "Sistema de Gestão Escolar Open Source";
-  const siteImage = "https://svelte.syki.com.br/logo-medium.png";
+  const siteImage = "https://svelte.syki.com.br/og_image.png";
   const siteUrl = url.href;
 
   return { siteTitle, siteDescription, siteImage, siteUrl };

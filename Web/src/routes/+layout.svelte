f8add49c1d4df16a22be12de16a04a6efcd40b1f
ZaqueuCavalcante <zaqueudovale@gmail.com>
2025-11-08 10:20:51 -0300

    Save

--- Web/src/routes/+layout.svelte
+++ Web/src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 
     let isDrawerOpen = $state(true);
 
+    import { page } from '$app/stores';
+
     let { data, children } = $props();
 </script>
 
@@ -30,21 +32,21 @@
       <ul class="menu w-full grow">
 
         <li>
-          <a href="/" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Home" aria-label="Home">
+          <a href="/" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" class:menu-active={$page.url.pathname === '/'} data-tip="Home" aria-label="Home">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="inline-block size-4 my-1.5"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
             <span class="is-drawer-close:hidden">Home</span>
           </a>
         </li>
 
         <li>
-          <a href="/academic/campi" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Campi" aria-label="Campi">
+          <a href="/academic/campi" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" class:menu-active={$page.url.pathname === '/academic/campi'} data-tip="Campi" aria-label="Campi">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="inline-block size-4 my-1.5"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
             <span class="is-drawer-close:hidden">Campi</span>
           </a>
         </li>
 
         <li>
-          <a href="/academic/disciplines" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Disciplinas" aria-label="Disciplinas">
+          <a href="/academic/disciplines" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" class:menu-active={$page.url.pathname === '/academic/disciplines'} data-tip="Disciplinas" aria-label="Disciplinas">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="inline-block size-4 my-1.5"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
             <span class="is-drawer-close:hidden">Disciplinas</span>
           </a>
